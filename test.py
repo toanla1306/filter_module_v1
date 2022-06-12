1b37eb49f607ea7dc14e82149e1108c35a60d1f6
toan la <ldtoan1306@gmail.com>
2022-06-12 23:17:11 +0700

    add test.py

--- test.py
+++ test.py
@@ -1,6 +1,1 @@
-print("Hello world")
-print("Test xung dot 2")
-print("Test xung dot note crawler")
-print("fix function A")
-print("Chen them truoc khi sua can pull ve")
-print("Test pull lan 2")
+test
